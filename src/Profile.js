397fccd755305d2b6e040d9b117b5ad4403ad4c3
SangeethaShanmugm <MY_NAME@example.com>
2023-03-15 13:35:36 +0530

    added bookList and router

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,6 @@
 import { Counter } from "./Counter";
 
-function Profile({ pic, name }) {
+export function Profile({ pic, name }) {
   return (
     <div className="user-container">
       <img className="profile-pic" src={pic} alt={name} />

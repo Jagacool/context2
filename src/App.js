d22cb67de0ab8fa4fb1e2d70ee23a60295668094
j <137029997+Jagacool@users.noreply.github.com>
2023-08-29 16:22:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,100 +1,65 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 import { AddColor } from "./AddColor";
-import { Home } from "./Home";
-import { Users } from "./Users";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { BookList } from "./BookList";
-import { BookDetail } from "./BookDetail";
-import { NotFoundPage } from "./NotFoundPage";
-import { AddBook } from "./AddBook";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import { ProductList } from "./ProductList";
+import { useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { ExampleContext } from "./ExampleContext";
-import { EditBook } from "./EditBook";
-import BasicForm from "./BasicForm";
-import { Tictactoe } from "./Tictactoe";
-const INITIAL_BOOK_LIST = [
+import { Home } from "./Home";
+import { UserList } from "./UserList";
+import { NotFoundPage } from "./NotFoundPage";
+import { ProductDetails } from "./ProductDetails";
+import { AddProduct } from "./AddProduct";
+import ExampleContext from "./context/ExampleContext";
+import TicTacToe from "./TicTacToe";
+import { EditProduct } from "./EditProduct";
+import { BasicForm } from "./BasicForm";
+
+export const INITIAL_PRODUCT_LIST = [
   {
-    name: "Charlotte's web",
-    poster:
-      "https://cdn.britannica.com/64/103064-050-295C6879/Charlottes-Web-EB-Garth-Williams.jpg",
-    rating: 8.8,
-    summary:
-      "The novel tells the story of Link livestock pig named Wilbur and his friendship with Link barn spider named Charlotte. When Wilbur is in danger of being slaughtered by the farmer, Charlotte writes messages praising Wilbur in her web in order to persuade the farmer to let him live.",
+    name: "Iphone 14",
+    price: "1,27,999",
+    poster: "https://m.media-amazon.com/images/I/71T5NVOgbpL._SX679_.jpg",
+    category: "Electronics",
+    rating: 8,
+    description:
+      "17.00 cm (6.7-inch) Super Retina XDR display featuring Always-On and ProMotion,48MP Main camera for up to 4x greater resolution",
   },
   {
-    name: "The power of your subconscious mind",
-    poster:
-      "https://kbimages1-a.akamaihd.net/6f3cf06c-4811-42d4-bd63-564c8264bc2d/1200/1200/False/the-power-of-your-subconscious-mind-57.jpg",
+    name: "Lenovo Tab M10 HD 2nd Gen",
+    price: "9,199",
+    poster: "https://m.media-amazon.com/images/I/71UXXKK2gSL._SX679_.jpg",
+    category: "Electronics",
+    rating: 8.7,
+    description:
+      "10.1 HD (1280x800) display with TDDI technology and 400 nits brightness and TUV Rhienland certified eye protection",
+    trailer: "https://www.youtube.com/embed/bbcpBC9Mbuk",
+  },
+  {
+    name: "U.S. POLO ASSN. Men T-Shirt",
+    price: "803",
+    poster: "https://m.media-amazon.com/images/I/819biZOXNrL._UX569_.jpg",
+    category: "Clothing Accessories",
     rating: 7,
-    summary:
-      "Your subconscious mind is Link powerful force to be reckoned with. It makes up around 95% of your brain power and handles everything your body needs to function properly, from eating and breathing to digesting and making memories",
-  },
-  {
-    name: "Attitude is everything ",
-    poster: "https://miro.medium.com/max/1400/1*ItFOYfi8Dyy0yj9n1SE8uQ.jpeg",
-    rating: 8.1,
-    summary:
-      "Attitude, In psychology, Link mental position with regard to Link fact or state. Attitudes reflect Link tendency to classify objects and events and to react to them with some consistency. Attitudes are not directly observable but rather are inferred from the objective, evaluative responses Link person makes.",
-  },
-  {
-    name: "The Secret",
-    poster: "https://m.media-amazon.com/images/I/81fdQIY6ykL.jpg",
-    summary:
-      "There's no secret to The Secret. The book and movie simply state that your thoughts control the universe. Through this “law of attraction” you “manifest” your desires. “It is exactly like placing an order from Link catalogue",
-    rating: 8.8,
-    trailer: "https://www.youtube.com/embed/Ma_qMbvTpzo",
-  },
-  {
-    name: "Discover Your Destiny",
-    rating: 6,
-    summary:
-      "'Discover Your Destiny' is Link story about enlightenment of Dar Sanderson, who is an incredibly ambitious executive. The book throws light on the fact that 'happiness and harmony can never be achieved and assured by SUCCESS'. Dar is an achiever in almost every aspect of life, yet he is void from the inside.",
-    poster: "https://m.media-amazon.com/images/I/61t18yWH5qL.jpg",
-  },
-  {
-    name: "The 5 AM Club",
-    poster: "https://m.media-amazon.com/images/I/71zytzrg6lL.jpg",
-    rating: 8.6,
-    summary:
-      "In The 5 AM Club: Own Your Morning. Elevate Your Life, he uses Link fictitious story about Link billionaire mentor teaching Link struggling artist and an entrepreneur about the importance of waking up early to show how revolutionary it is for success.",
-  },
-  {
-    name: "Atomic Habits",
-    poster: "https://m.media-amazon.com/images/I/91bYsX41DVL.jpg",
-    rating: 8,
-    summary:
-      "Atomic Habits is the definitive guide to breaking bad behaviors and adopting good ones in four steps, showing you how small, incremental, everyday routines compound into massive, positive change over time.",
-  },
-  {
-    name: "I Can Do It",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/81T3L1yTJwL.jpg",
-    rating: 8,
-    summary:
-      "Hay shows you that you “can do it”—that is, change and improve virtually every aspect of your life—by understanding and using affirmations correctly. Louise explains that every thought you think and every word you speak is an affirmation. Even your self-talk, your internal dialogue, is Link stream of affirmations.",
+    description: "Fit Type: Regular Fit, HALF SLEEVE POLO",
   },
 ];
 
 export default function App() {
-  const [bookList, setBookList] = useState([]);
-  const navigate = useNavigate();
-  //1. Creating -  createContext ✅
-  //2. Publisher - Provider  - context.Provider ✅
-  //3. Subscriber  - useContext - useContext(context) ❌
   const [mode, setMode] = useState("light");
+
   const theme = createTheme({
     palette: {
       mode: mode,
     },
   });
+
+  const navigate = useNavigate();
 
   return (
     <ThemeProvider theme={theme}>
@@ -105,29 +70,32 @@ export default function App() {
             <Button color="inherit" onClick={() => navigate("/")}>
               Home
             </Button>
-            <Button color="inherit" onClick={() => navigate("/books")}>
-              BookList
+            <Button color="inherit" onClick={() => navigate("/products")}>
+              ProductList
             </Button>
-            <Button color="inherit" onClick={() => navigate("/add-color")}>
+            <Button color="inherit" onClick={() => navigate("/products/add")}>
+              Add Product
+            </Button>
+            <Button color="inherit" onClick={() => navigate("/color-game")}>
               AddColor
             </Button>
-            <Button color="inherit" onClick={() => navigate("/profile")}>
-              Users
+            <Button color="inherit" onClick={() => navigate("/users")}>
+              UserList
             </Button>
-            <Button color="inherit" onClick={() => navigate("/books/add")}>
-              AddBook
+            <Button color="inherit" onClick={() => navigate("/somewhere")}>
+              SomeWhere
             </Button>
             <Button color="inherit" onClick={() => navigate("/context")}>
               Context
             </Button>
-            <Button color="inherit" onClick={() => navigate("/form")}>
+            <Button color="inherit" onClick={() => navigate("/tic-tac-toe")}>
+              TicTacToe
+            </Button>
+            <Button color="inherit" onClick={() => navigate("/basic-form")}>
               Basic Form
             </Button>
-            <Button color="inherit" onClick={() => navigate("/game")}>
-              Tic-Tac-Toe
-            </Button>
             <Button
-              sx={{ marginLeft: "auto" }}
+              sx={{ marginLeft: "10%" }}
               startIcon={
                 mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />
               }
@@ -138,21 +106,20 @@ export default function App() {
             </Button>
           </Toolbar>
         </AppBar>
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<BookList />} />
-          <Route path="/books/:bookid" element={<BookDetail />} />
-          <Route path="/books/add" element={<AddBook />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/items" element={<Navigate replace to="/products" />} />
+          <Route path="/products/add" element={<AddProduct />} />
+          <Route path="/color-game" element={<AddColor />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/products/:productid" element={<ProductDetails />} />
 
-          <Route path="/books/edit/:bookid" element={<EditBook />} />
+          <Route path="/products/edit/:productid" element={<EditProduct />} />
 
-          <Route path="/add-color" element={<AddColor />} />
-          <Route path="/profile" element={<Users />} />
           <Route path="/context" element={<ExampleContext />} />
-          <Route path="/form" element={<BasicForm />} />
-          <Route path="/game" element={<Tictactoe />} />
-          <Route path="/novel" element={<Navigate replace to="/books" />} />
+          <Route path="/tic-tac-toe" element={<TicTacToe />} />
+          <Route path="/basic-form" element={<BasicForm />} />
           <Route path="/404" element={<NotFoundPage />} />
           <Route path="*" element={<Navigate replace to="/404" />} />
         </Routes>
@@ -160,5 +127,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
-
